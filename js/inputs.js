b810d7a5296cc587feb13a95aa14cded0a0967b0
Yggy <igor.yentaltsev@gmail.com>
2013-11-25 08:33:16 -0500

    Added js/ and filled up index.html

---
+++ js/inputs.js
@@ -0,0 +1,11 @@
+//inputs.js
+var high = [
+	"Quality-oriented, self-motivated, resourceful, highly adaptive worker",
+	"Committed to teamwork, proven team player, efficient leader with proven managerial skills",
+	"Task-focused, tenacious, hard-working, success-oriented, analytical thinker",
+	"Committed to continuous learning, skill development", 
+	"Multicultural experience, knowledge of several languages (Russian, Mandarin, German)"
+];
+
+var a = "AAAA";
+document.write("hello!!");
